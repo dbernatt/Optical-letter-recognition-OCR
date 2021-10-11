2765ce91d4f79de3836bf78b3e6ca06b292fdde8
Domokos Bernat <domberills@gmail.com>
2021-10-11 06:25:44 +0300

    added readme

--- testknn.m
+++ testknn.m
@@ -2,7 +2,7 @@ function testknn(k)
 
   traindata = dlmread('optdigits.tra', ',', 0, 0);
   testdata = dlmread('optdigits.tes', ',', 0, 0);
-  %train_accuracy = knn(traindata, traindata, k)
+  train_accuracy = knn(traindata, traindata, k)
   test_accuracy = knn(traindata, testdata, k)
 end
  
